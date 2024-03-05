111da34d46c392425ad3955b97f9b809be2df118
ilich valbuena <ifvalbuena@indracompany.com>
2024-03-05 16:10:05 -0500

    descargar .zip

--- App.js
+++ App.js
@@ -1,20 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import JSZipUtils from "jszip-utils";
+import JSZip from "jszip";
+import GetStories from "./src/core";
 
+
+
+const _url = "https://git.door43.org/es-419_gl/xsu_obs/archive/master.zip";
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  console.log(GetStories(_url));
+  return null;
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
