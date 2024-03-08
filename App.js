175c7f4f5d41d24def41a20524a2821e4e19d412
eliaspinero <eliasrpt14@gmail.com>
2024-03-08 00:53:35 -0500

    newStyle

--- App.js
+++ App.js
@@ -1,8 +1,6 @@
 import { StatusBar } from "expo-status-bar";
-import { Platform, Pressable, StyleSheet, Text, View } from "react-native";
-import { useEffect, useState } from "react";
-import { shareAsync } from "expo";
-import * as FileSystem from "expo-file-system";
+import { StyleSheet, View } from "react-native";
+import { useEffect } from "react";
 import { OBSContextProvider, useObsNav, useObs } from "./GlobalState";
 
 import { StoryNav } from "./src/components/StoryNav";
@@ -28,59 +26,6 @@ function Test() {
     setSrc(_url);
   }, [_url]);
 
-  function download() {
-    const blob = new Blob([JSON.stringify(source.stories)], {
-      type: "application/json",
-    });
-
-    saveFile(
-      FileSystem.documentDirectory,
-      "obs.json",
-      "application/json",
-      JSON.stringify(source.stories)
-    );
-  }
-
-  async function readDirectory() {
-    console.log("readDirectory,");
-    const base64 = await FileSystem.readAsStringAsync(
-      "content://com.android.providers.downloads.documents/document"
-    );
-    console.log("base64,", base64);
-  }
-
-  async function saveFile(uri, filename, mimetype, fileContent) {
-    console.log("entro a saveFile");
-    if (Platform.OS === "android") {
-      console.log("entro a android");
-      console.log("mimetype", mimetype);
-      const permissions =
-        await FileSystem.StorageAccessFramework.requestDirectoryPermissionsAsync();
-
-      if (permissions.granted) {
-        console.log("entro a granted");
-
-        await FileSystem.StorageAccessFramework.createFileAsync(
-          permissions.directoryUri,
-          filename,
-          mimetype
-        )
-          .then(async (uri) => {
-            console.log("emtro a uri");
-            await FileSystem.writeAsStringAsync(uri, fileContent, {
-              encoding: FileSystem.EncodingType.UTF8,
-            });
-          })
-          .catch((e) => console.log(e));
-      } else {
-        console.log("entro a else");
-        shareAsync(uri);
-      }
-    } else {
-      console.log("entro a else2");
-      shareAsync(uri);
-    }
-  }
 
   return source ? (
     <View style={styles.storyContainer}>
@@ -92,12 +37,6 @@ function Test() {
         onSelect={goTo}
       ></StoryNav>
       <FrameObs text={getFrameTextFromRef(reference)} image={image}></FrameObs>
-      <Pressable style={styles.button} onPress={download}>
-        <Text>Guardar</Text>
-      </Pressable>
-      <Pressable style={styles.button} onPress={readDirectory}>
-        <Text>leer</Text>
-      </Pressable>
       <FrameNav></FrameNav>
     </View>
   ) : null;
--- src/components/StoryNav.js
+++ src/components/StoryNav.js
@@ -2,9 +2,9 @@ import { Picker } from "@react-native-picker/picker";
 
 export function StoryNav({ selectedStory, stories, onSelect }) {
   return (
-    <Picker selectedValue={selectedStory} onValueChange={onSelect}>
+    <Picker selectedValue={selectedStory} onValueChange={onSelect} style={{ marginTop: 30}} multiline={true} numberOfLines={3}>
       {stories.map((title, key) => {
-        return <Picker.Item label={title} value={key + 1} key={key + 1} />;
+        return <Picker.Item label={title} value={key + 1} key={key + 1} style={{ fontSize: 28 }}/>;
       })}
     </Picker>
   );
